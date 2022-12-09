ae54fc66b314ffeebfaf78b83c5b647a6bf31543
ShingoYadomoto <shingo.yadomoto@gmail.com>
2022-12-10 01:41:13 +0900

    add calulate outline

--- hand.go
+++ hand.go
@@ -14,11 +14,11 @@ func NewHupaiCalculater(standard StandardHoluPattern, zhuangfeng Zhuangfeng, zif
 	}
 }
 
-func (hc HupaiCalculater) menzen() HandType {
+func (hc HupaiCalculater) menzen() []HandType {
 	if hc.standard.IsMenzen() {
-		return 門前清自摸和
+		return []HandType{門前清自摸和}
 	}
-	return 0
+	return []HandType{}
 }
 
 func (hc HupaiCalculater) fengpai() []HandType {
@@ -41,6 +41,31 @@ func (hc HupaiCalculater) fengpai() []HandType {
 	return hands
 }
 
+func (hc HupaiCalculater) pinfu() []HandType          { panic("not implemented") }
+func (hc HupaiCalculater) tanyao() []HandType         { panic("not implemented") }
+func (hc HupaiCalculater) ipeko() []HandType          { panic("not implemented") }
+func (hc HupaiCalculater) sansyokuDoujun() []HandType { panic("not implemented") }
+func (hc HupaiCalculater) ittu() []HandType           { panic("not implemented") }
+func (hc HupaiCalculater) chanta() []HandType         { panic("not implemented") }
+func (hc HupaiCalculater) toitoi() []HandType         { panic("not implemented") }
+func (hc HupaiCalculater) sanAnko() []HandType        { panic("not implemented") }
+func (hc HupaiCalculater) sanKantsu() []HandType      { panic("not implemented") }
+func (hc HupaiCalculater) sansyokuDoko() []HandType   { panic("not implemented") }
+func (hc HupaiCalculater) honro() []HandType          { panic("not implemented") }
+func (hc HupaiCalculater) syosangen() []HandType      { panic("not implemented") }
+func (hc HupaiCalculater) honitsu() []HandType        { panic("not implemented") }
+func (hc HupaiCalculater) junchan() []HandType        { panic("not implemented") }
+func (hc HupaiCalculater) ryanpeko() []HandType       { panic("not implemented") }
+func (hc HupaiCalculater) tinitsu() []HandType        { panic("not implemented") }
+func (hc HupaiCalculater) suAnko() []HandType         { panic("not implemented") }
+func (hc HupaiCalculater) daisangen() []HandType      { panic("not implemented") }
+func (hc HupaiCalculater) sushi() []HandType          { panic("not implemented") }
+func (hc HupaiCalculater) tsuiso() []HandType         { panic("not implemented") }
+func (hc HupaiCalculater) ryuiso() []HandType         { panic("not implemented") }
+func (hc HupaiCalculater) chinro() []HandType         { panic("not implemented") }
+func (hc HupaiCalculater) suKantsu() []HandType       { panic("not implemented") }
+func (hc HupaiCalculater) churen() []HandType         { panic("not implemented") }
+
 type FullHupaiCalculater struct {
 	fullParrern FullHoluPattern
 	zhuangfeng  Zhuangfeng
@@ -49,16 +74,40 @@ type FullHupaiCalculater struct {
 
 func (fhc FullHupaiCalculater) Hupai() AllHands {
 	all := AllHands{}
-	all = append(all, fhc.kokushi()...)
-	all = append(all, fhc.titoitsu()...)
+	all = append(all, fhc.kokushi()...)  //	国士無双・国士無双十三面
+	all = append(all, fhc.titoitsu()...) // 七対子
 	for _, standard := range fhc.fullParrern.Standard {
 		calculater := NewHupaiCalculater(standard, fhc.zhuangfeng, fhc.zifeng)
 
-		all = append(all, calculater.menzen())     // 門前
-		all = append(all, calculater.fengpai()...) // 場風・自風・白・發・中
+		all = append(all, calculater.menzen()...)         // 門前
+		all = append(all, calculater.fengpai()...)        // 場風・自風・白・發・中
+		all = append(all, calculater.pinfu()...)          //平和
+		all = append(all, calculater.tanyao()...)         //断幺九
+		all = append(all, calculater.ipeko()...)          //一盃口
+		all = append(all, calculater.sansyokuDoujun()...) //三色同順
+		all = append(all, calculater.ittu()...)           //一気通貫
+		all = append(all, calculater.chanta()...)         //混全帯幺九
+		all = append(all, calculater.toitoi()...)         //対々和
+		all = append(all, calculater.sanAnko()...)        //三暗刻
+		all = append(all, calculater.sanKantsu()...)      //三槓子
+		all = append(all, calculater.sansyokuDoko()...)   //三色同刻
+		all = append(all, calculater.honro()...)          //混老頭
+		all = append(all, calculater.syosangen()...)      //小三元
+		all = append(all, calculater.honitsu()...)        //混一色
+		all = append(all, calculater.junchan()...)        //純全帯幺九
+		all = append(all, calculater.ryanpeko()...)       //二盃口
+		all = append(all, calculater.tinitsu()...)        //清一色
+		all = append(all, calculater.suAnko()...)         //四暗刻・四暗刻単騎
+		all = append(all, calculater.daisangen()...)      //大三元
+		all = append(all, calculater.sushi()...)          //小四喜・大四喜
+		all = append(all, calculater.tsuiso()...)         //字一色
+		all = append(all, calculater.ryuiso()...)         //緑一色
+		all = append(all, calculater.chinro()...)         //清老頭
+		all = append(all, calculater.suKantsu()...)       //四槓子
+		all = append(all, calculater.churen()...)         //九蓮宝燈・純正九蓮宝燈
 	}
 
-	return nil
+	return all
 }
 
 func (fhc FullHupaiCalculater) kokushi() AllHands {
